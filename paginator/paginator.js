14c5cd8461edfd518aad501b7daacf368d365e1d
Arikaim <sales@intersoft.biz>
2020-07-16 18:12:42 +0300

    =

--- paginator/paginator.js
+++ paginator/paginator.js
@@ -4,39 +4,53 @@
  *  @license    http://www.arikaim.com/license
  *  http://www.arikaim.com
  */
-"use strict";
+'use strict';
 
 function Paginator() {
     var self = this;    
     this.options = {};
-    
-    this.init = function(rowsId, component, namespace) {             
+    this.buttons = {};
+    this.currentPage = 1;
+
+    this.init = function(rowsId, component, namespace, buttons) {             
         component = getDefaultValue(component,$('#' + rowsId).attr('component'));
         namespace = getDefaultValue(namespace,$('#' + rowsId).attr('namespace'));
-   
+        if (isEmpty(buttons) == true) {
+            this.buttons.selector = 'paginator';
+            this.buttons.component = 'components:paginator';
+            this.buttons.params = {};
+        } else {
+            this.buttons = buttons;
+        }
         this.setOptions(rowsId,component,namespace);
+        this.initButtons();
 
-        arikaim.ui.button('.page-link',function(element) {
-            var page = $(element).attr('page'); 
-            return self.setPage(page,self.getOptions().namespace,function(result) {               
-                arikaim.events.emit('paginator.load.page',result);
-                self.initButtons(result.page);                                
-                self.loadRows(self.getOptions());  
-            }); 
-        });
-       
         $('.page-size-menu').dropdown({
             onChange: function(value) {               
                 self.setPageSize(value,self.getOptions().namespace,function(pageSize) {
                     arikaim.events.emit('paginator.page.size.changed',pageSize);
-                    self.loadRows(self.getOptions());
+                    self.setPage(1,self.getOptions().namespace,function(result) {               
+                        arikaim.events.emit('paginator.load.page',result);                                                     
+                        self.loadRows(function(result) {
+                            self.reload();  
+                        });  
+                    });                                     
                 });                           
             }
         });
     };
 
     this.initButtons = function(page) {
-        page = parseInt(page);
+        page = (isEmpty(page) == true) ? this.currentPage : parseInt(page);
+        arikaim.ui.button('.page-link',function(element) {
+            var page = $(element).attr('page'); 
+            return self.setPage(page,self.getOptions().namespace,function(result) {            
+                arikaim.events.emit('paginator.load.page',result);
+                self.initButtons();                                
+                self.loadRows();  
+            }); 
+        });
+
         $('.page-link').removeClass('active');       
         $('.page-' + page).addClass('active');    
         
@@ -67,18 +81,18 @@ function Paginator() {
         }
     };
 
+    this.setParams = function(params) {
+        this.options.params = params;
+    };
+
     this.setOptions = function(rowsId, component, namespace) {
-        namespace = getDefaultValue(namespace,"");
+        namespace = getDefaultValue(namespace,'');
         this.options = { 
             id: rowsId,           
             namespace: namespace       
-        };
-        if (isString(component) == true) {
-            this.options.component = component;
-        } else {
-            this.options.component = component.name;
-            this.options.params = component.params;
-        }
+        };      
+        this.options.component = (isString(component) == true) ? component : component.name;
+        this.options.params = (isObject(component.params) == true) ? component.params : {};
     };
 
     this.getOptions = function() {      
@@ -87,7 +101,7 @@ function Paginator() {
 
     this.getCurrentPage = function(namespace, onSuccess, onError) {
         var deferred = new $.Deferred();
-        namespace = getDefaultValue(namespace,"");
+        namespace = getDefaultValue(namespace,'');
 
         arikaim.get('/core/api/ui/paginator/' + namespace,function(result) {
             deferred.resolve(result.page);
@@ -105,7 +119,10 @@ function Paginator() {
 
         pageSize = (isEmpty(pageSize) == true) ? 1 : pageSize;
         namespace = getDefaultValue(namespace,"");
-        var data = { page_size: pageSize, namespace: namespace };
+        var data = { 
+            page_size: pageSize,
+            namespace: namespace 
+        };
 
         arikaim.put('/core/api/ui/paginator/page-size',data,function(result) {
             deferred.resolve(result.page_size);  
@@ -133,6 +150,7 @@ function Paginator() {
             namespace: namespace 
         };
         arikaim.put('/core/api/ui/paginator/page',data,function(result) {
+            self.currentPage = result.page;
             deferred.resolve(result.page);  
             callFunction(onSuccess,result);      
         },function(error) {
@@ -143,27 +161,27 @@ function Paginator() {
         return deferred.promise();
     };
 
-    this.loadRows = function(options) { 
-        if (isObject(options) == false) {
-            return false;
-        }
-        
+    this.loadRows = function(onSuccess) {  
         return arikaim.page.loadContent({
-            id: options.id,
-            component: options.component,
-            params: options.params
+            id: self.options.id,
+            component: self.options.component,
+            params: (isEmpty(self.options.params) == true) ? {} : self.options.params
+        },function(result) {
+            callFunction(onSuccess,result);
         });
     };
 
     this.reload = function(selector, component) {
-        selector = getDefaultValue(selector,'paginator');
-        component = getDefaultValue(component,'components:paginator');
+        selector = getDefaultValue(selector,this.buttons.selector);
+        component = getDefaultValue(component,this.buttons.component);
+        this.buttons.params.namespace = self.options.namespace;
+      
         return arikaim.page.loadContent({
             id: selector,
             component: component,
-            params: { namespace: self.options.namespace }
-        },function(result) {
-            self.init(self.options.id,self.options.component,self.options.namespace)
+            params: self.buttons.params
+        },function(result) {            
+            self.initButtons();            
         });
     }
 }
