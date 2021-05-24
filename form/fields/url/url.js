c7c1f9444222f477ebf32955e66b1e1bcc00f691
Arikaim <sales@intersoft.biz>
2021-05-24 16:06:20 +0300

    =

--- form/fields/url/url.js
+++ form/fields/url/url.js
@@ -1,6 +1,8 @@
 'use stric';
 
 arikaim.component.onLoaded(function() {
+    $('.http-schema-dropdown').dropdown({});
+
     arikaim.ui.button('.clear-url',function(element) {
         var fieldId = $(element).attr('url-field');
         $('#' + fieldId).val('');
