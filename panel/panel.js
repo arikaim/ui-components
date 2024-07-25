05474a15000d8625e05d635f9f08b3d397493435
Arikaim <info@arikaim.com>
2024-07-25 13:37:35 +0300

    =

--- panel/panel.js
+++ panel/panel.js
@@ -2,11 +2,21 @@
 
 arikaim.component.onLoaded(function(component) {
 
+    component.close = function() {
+        if (component.get('show_parent') != true) {
+            $(component.getElement()).parent().hide();     
+        }
+        $(component.getElement()).remove();             
+    };
+    
+    component.remove = function() {
+        $(component.getElement()).remove();    
+    }
+
     component.init = function() {
         var closeButtons = $(component.getElement()).find('.panel-close-button');
-        arikaim.ui.button(closeButtons,function(element) { 
-            $(component.getElement()).parent().hide();     
-            $(component.getElement()).remove();             
+        arikaim.ui.button(closeButtons,function() { 
+            component.close();
         });
         $(component.getElement()).parent().show();
     };
